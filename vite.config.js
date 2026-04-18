8ffdb33234eca02e7b7530c80c29a6d9f4dfc736
lichiahao-frank <lichiahao.frank@gmail.com>
2026-04-18 14:47:30 +0800

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // 請確保這裡沒有 base: '/xxx/' 的設定
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
